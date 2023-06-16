18d3ec45f25b82ff3d43337e30373fac78096809
Johannes Scheuerer <joscheuerer@gmail.com>
2023-06-16 13:06:17 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,12 +2,6 @@
 # VPC
 ################################################################################
 
-variable "create_vpc" {
-  description = "Controls if VPC should be created (it affects almost all resources)"
-  type        = bool
-  default     = true
-}
-
 variable "name" {
   description = "Name to be used on all the resources as identifier"
   type        = string
@@ -17,6 +11,18 @@ variable "cidr" {
   description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
   type        = string
   default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = ["eu-central-1a","eu-central-1b"]
+}
+
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  type        = bool
+  default     = true
 }
 
 variable "secondary_cidr_blocks" {
@@ -29,12 +35,6 @@ variable "instance_tenancy" {
   description = "A tenancy option for instances launched into the VPC"
   type        = string
   default     = "default"
-}
-
-variable "azs" {
-  description = "A list of availability zones names or ids in the region"
-  type        = list(string)
-  default     = ["eu-central-1a","eu-central-1b"]
 }
 
 variable "enable_dns_hostnames" {
